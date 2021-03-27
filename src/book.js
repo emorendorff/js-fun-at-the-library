49118429d44964b27a324501e50f8c436994b156
Em Orendorff <em.orendorff@gmail.com>
2021-03-27 12:07:04 -0500

    Complete book.js functions and edit book-test for errors.

--- src/book.js
+++ src/book.js
@@ -1,12 +1,46 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`;
+
+};
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+  return character
+};
+
+function saveReview(content,reviews) {
+  if (reviews.indexOf(content) === -1) {
+    reviews.push(content);
+  }
+};
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20
+};
+
+function writeBook(title, character, genre) {
+  var book = {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre:genre,
+  }
+  return book
+};
+
+function editBook(book) {
+  var newPageCount = book.pageCount * 0.75
+  book.pageCount = newPageCount 
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
